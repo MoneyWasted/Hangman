70e095ae6ae1008fa57c78e3149cf9bd373a6c3a
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2021-04-20 09:42:36 -0400

    Handling Key Board Inputs

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,10 @@
 #include <windows.h>
 #include <d2d1.h>
 #include <wincodec.h>
+#include <dwrite.h>
 #include "basewin.h"
+#include <vector>
+#include <string>
 
 #pragma comment(lib, "d2d1")
 using namespace D2D1;
@@ -18,6 +21,8 @@ template <class T> void SafeRelease(T** ppT)
 class MainWindow : public BaseWindow<MainWindow>
 {
     ID2D1Factory*           pFactory;
+    IDWriteFactory*         pDWriteFactory;
+    IDWriteTextFormat*      pTextFormat;
     ID2D1HwndRenderTarget*  pRenderTarget;
     ID2D1SolidColorBrush*   pBrush;
     ID2D1SolidColorBrush*   GroundColorBrush;
@@ -50,6 +55,28 @@ void MainWindow::CalculateLayout()
     }
 }
 
+std::string input;
+std::string HWNDToString(HWND inputA)
+{
+    std::string s;
+    int len = GetWindowTextLength(inputA);
+    if (len > 0)
+    {
+        s.resize(len + 1);
+        len = GetWindowText(inputA, (LPWSTR)&s[0], s.size());
+        s.resize(len);
+    }
+    return s;
+}
+
+const wchar_t* String2WCHAR(std::string string)
+{
+    std::string narrow_string(string);
+    std::wstring wide_string = std::wstring(narrow_string.begin(), narrow_string.end());
+    const wchar_t* result = wide_string.c_str();
+    return result;
+}
+
 HRESULT MainWindow::CreateGraphicsResources()
 {
     HRESULT hr = S_OK;
@@ -59,11 +86,14 @@ HRESULT MainWindow::CreateGraphicsResources()
         GetClientRect(m_hwnd, &rc);
 
         D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+        static const WCHAR msc_fontName[] = L"Verdana";
+        static const FLOAT msc_fontSize = 50;
 
         hr = pFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(m_hwnd, size), &pRenderTarget);
 
         if (SUCCEEDED(hr))
         {
+
             const D2D1_COLOR_F MainColor = D2D1::ColorF(1.0f, 1.0f, 0);
             hr = pRenderTarget->CreateSolidColorBrush(MainColor, &pBrush);
 
@@ -75,6 +105,23 @@ HRESULT MainWindow::CreateGraphicsResources()
 
             const D2D1_COLOR_F GrassColor = D2D1::ColorF(0.28f, 0.72f, 0.20f, 1.0f);
             hr = pRenderTarget->CreateSolidColorBrush(GrassColor, &GrassColorBrush);
+
+            hr = DWriteCreateFactory(
+                DWRITE_FACTORY_TYPE_SHARED,
+                __uuidof(pDWriteFactory),
+                reinterpret_cast<IUnknown**>(&pDWriteFactory)
+            );
+
+            hr = pDWriteFactory->CreateTextFormat(
+                msc_fontName,
+                NULL,
+                DWRITE_FONT_WEIGHT_NORMAL,
+                DWRITE_FONT_STYLE_NORMAL,
+                DWRITE_FONT_STRETCH_NORMAL,
+                msc_fontSize,
+                L"", //locale
+                &pTextFormat
+            );
 
             if (SUCCEEDED(hr)) { CalculateLayout(); }
         }
@@ -91,6 +138,7 @@ void MainWindow::DiscardGraphicsResources()
 void MainWindow::OnPaint()
 {
     HRESULT hr = CreateGraphicsResources();
+
     if (SUCCEEDED(hr))
     {
         PAINTSTRUCT ps;
@@ -111,6 +159,14 @@ void MainWindow::OnPaint()
         pRenderTarget->DrawLine(Point2F(50, 100), Point2F(50, 500), GallowColorBrush, 20.0f, NULL);
         pRenderTarget->DrawLine(Point2F(10, 500), Point2F(90, 500), GallowColorBrush, 20.0f, NULL);
         pRenderTarget->DrawLine(Point2F(140, 100), Point2F(50, 200), GallowColorBrush, 20.0f, NULL);
+
+        pRenderTarget->DrawText(
+            (WCHAR*)input.c_str(),
+            input.length(),
+            pTextFormat,
+            D2D1::RectF(0, 527, pRenderTarget->GetSize().width, pRenderTarget->GetSize().height),
+            GallowColorBrush
+        );
 
         // Draw the man
         ellipse = D2D1::Ellipse(D2D1::Point2F(250, 175), 45, 45);
@@ -188,6 +244,12 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
     case WM_SIZE:
         Resize();
         return 0;
+
+    case WM_CHAR:
+        if (LOWORD(wParam) == 1)
+        {
+            input = HWNDToString(m_hwnd);
+        }
     }
     return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
 }
